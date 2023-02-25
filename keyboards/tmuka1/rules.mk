2d9a514cd432cea1a105447817d8ae0e84bd769c
Tony Muka <tonymuka@gmail.com>
2023-02-25 00:48:36 -0500

    latest

--- keyboards/tmuka1/rules.mk
+++ keyboards/tmuka1/rules.mk
@@ -1,6 +1,6 @@
 #BOARD = GENERIC_RP_RP2040
-PS2_MOUSE_ENABLE = yes
-PS2_ENABLE = yes
-PS2_DRIVER = usart
+#PS2_MOUSE_ENABLE = yes
+#PS2_ENABLE = yes
+#PS2_DRIVER = usart
 
 MOUSEKEY_ENABLE = yes  # Mouse keys
