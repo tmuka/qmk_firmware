b31fa47fa30dd917fe5d8d3e11cb8e25cb1fd790
Tony Muka <tonymuka@gmail.com>
2023-02-25 00:53:45 -0500

    relocated and finally got to compile

--- keyboards/handwired/tmuka1/config.h
+++ keyboards/handwired/tmuka1/config.h
@@ -3,6 +3,16 @@
 
 #pragma once
 
+#undef VENDOR_ID
+#undef PRODUCT_ID
+#undef DEVICE_VER
+#undef MANUFACTURER
+#undef PRODUCT
+#define VENDOR_ID       0xFEED
+#define PRODUCT_ID      0x4096
+#define DEVICE_VER      0x0001
+#define MANUFACTURER    "Nobody"
+#define PRODUCT         "tmuka keyboard"
 #include "config_common.h"
 
 #define DEBUG_MATRIX_SCAN_RATE
@@ -13,4 +23,5 @@
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_LED GP25
 #define RP2040_BOOTLOADER_DOUBLE_TAP_RESET_TIMEOUT 500U
+
 
