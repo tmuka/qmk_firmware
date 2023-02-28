cbced1c4e9437805d5e1380c486d275cd696cab0
Tony Muka <tonymuka@gmail.com>
2023-02-28 10:18:54 -0500

    added spacecadet config to keymap

--- keyboards/handwired/tmuka1/rules.mk
+++ keyboards/handwired/tmuka1/rules.mk
@@ -1,6 +1,9 @@
 BOOTLOADER = rp2040
 MOUSEKEY_ENABLE = yes
 
+#disabe command to fix spacecadet issues with holding both shift keys
+COMMAND_ENABLE = no
+
 # these dont seem to work yet with rp2040
 #PS2_MOUSE_ENABLE = yes
 #PS2_ENABLE = yes
